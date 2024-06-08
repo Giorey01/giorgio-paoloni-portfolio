4646f3ff89710df0c6f74ee173ff285f8f468bcd
Giorey01 <giorey2001@gmail.com>
2024-06-08 21:23:39 +0200

    Make main portfolio pages directories

---
+++ src/app/about/page.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function About() {
+  return (
+    <div className="py-5 px-10 text-justify">
+      <h1 className="font-bold text-3xl text-center mb-3">Lorem</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti, iste
+        modi nulla ex nemo exercitationem neque perferendis necessitatibus
+        accusamus, omnis tempore, sapiente architecto? Hic velit ratione
+        laboriosam! Recusandae consequatur quod molestias, debitis ut magni
+        molestiae totam repudiandae non doloremque cumque porro exercitationem,
+        accusamus ipsam iure! Inventore delectus placeat possimus totam saepe
+        voluptates quam minus veniam velit, temporibus dolorum accusantium quo
+        earum, tenetur repellendus amet perferendis explicabo. Cum provident
+        consequuntur veniam dolore, ad quaerat tenetur iure laudantium maxime,
+        hic, sit corrupti. Autem odit odio iste? Tenetur eaque labore quod
+        molestiae in, ducimus atque natus sint enim possimus dolor beatae et.
+        Non
+      </p>
+    </div>
+  );
+}
+
+export default About;
