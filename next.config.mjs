a5846e48197444b7e245b2f4351f99b3b1cb15fb
Giorey01 <giorey2001@gmail.com>
2024-08-11 20:43:17 +0200

    Add aws s3 source

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "giorgio-paoloni-gallery-storage.s3.eu-north-1.amazonaws.com",
+        pathname: "**", // Matches any path within the bucket
+      },
+    ],
+  },
+};
 export default nextConfig;
