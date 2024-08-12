5ea2f969fa75f60ed8866298823f0fbff6377ae1
Giorey01 <giorey2001@gmail.com>
2024-08-12 08:41:19 +0200

    Fix main menu not appearing while in blog menu + change transition to main menu

--- src/app/header.jsx
+++ src/app/header.jsx
@@ -11,40 +11,43 @@ function Header() {
   return (
     <>
       <div className="flex items-center justify-between px-7 mt-4 h-10 w-full">
-        <a
+        <Link
           href="https://www.instagram.com/_jojifilm_/"
-          rel="noopener noreferrer "
+          rel="noopener noreferrer"
           target="_blank"
         >
           <FaInstagram className="scale-150 cursor-pointer" />
-        </a>
-        <RxHamburgerMenu
+        </Link>
+        <button
+          aria-expanded={isNavOpen}
           className="scale-150 cursor-pointer h-16"
           onClick={() => setIsNavOpen((prev) => !prev)}
-        />
+        >
+          <RxHamburgerMenu />
+        </button>
       </div>
 
       <div
-        className={`absolute w-full h-full top-0 left-0 bg-white flex flex-col justify-evenly items-center z-10 transition-opacity duration-300 ${
-          isNavOpen
-            ? "opacity-100 pointer-events-auto"
-            : "opacity-0 pointer-events-none"
+        className={`fixed inset-0 bg-white flex flex-col justify-center items-center z-10 transform transition-transform duration-300 ${
+          isNavOpen ? "translate-x-0" : "translate-x-full"
         }`}
       >
         <div
-          className="fixed top-0 flex items-center justify-between px-7 mt-4 h-10 w-full"
+          className="absolute top-0 flex items-center justify-between px-7 mt-4 h-10 w-full"
           onClick={() => setIsNavOpen(false)}
         >
-          <a
+          <Link
             href="https://www.instagram.com/_jojifilm_/"
             rel="noopener noreferrer"
             target="_blank"
           >
             <FaInstagram className="scale-150 cursor-pointer" />
-          </a>
-          <RxCross2 className="cursor-pointer scale-150 h-16" />
+          </Link>
+          <button className="cursor-pointer scale-150 h-16">
+            <RxCross2 />
+          </button>
         </div>
-        <ul className="flex flex-col items-center justify-center  min-h-[250px] text-2xl">
+        <ul className="flex flex-col items-center justify-center min-h-[250px] text-2xl">
           <li className="border-b border-gray-400 my-8 uppercase">
             <Link href="/" onClick={() => setIsNavOpen(false)}>
               Home
