4440cbffaac9a5bf2fbb8555a967568f351daab2
Giorey01 <giorey2001@gmail.com>
2024-08-13 20:57:16 +0200

    Add generation of each portfolio page

---
+++ src/app/portfolio/[slug]/page.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { getImagesFromFolder } from "@/utils/awsS3UtilityFunctions";
+import Image from "next/image";
+
+interface PortfolioPageProps {
+  params: {
+    slug: string;
+  };
+}
+
+const PortfolioPage = async ({ params }: PortfolioPageProps) => {
+  const { slug } = params;
+  const decodedSlug = decodeURIComponent(slug);
+  const images = await getImagesFromFolder(
+    "Portfolio/" + decodedSlug.charAt(0).toUpperCase() + decodedSlug.slice(1)
+  );
+  console.log(images);
+  return (
+    <div className="p-4 grid grid-cols-1 sm:grid-cols-2 gap-4">
+      {images?.map((image, index) => (
+        <div className="relative w-full overflow-hidden rounded-lg">
+          <Image
+            key={index}
+            src={`https://giorgio-paoloni-gallery-storage.s3.eu-north-1.amazonaws.com/${image.Key}`}
+            alt={image.Key ?? ""}
+            width={800}
+            height={800}
+            className="rounded-md object-cover w-full h-full"
+          ></Image>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default PortfolioPage;
--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,16 +1,10 @@
 import React from "react";
 import {
   getFoldersInFolder,
-  getFirstImageInFolder,
+  getFirstImageFromFolder,
 } from "@/utils/awsS3UtilityFunctions";
 import Image from "next/image";
-interface S3Object {
-  Key: string;
-  LastModified: Date;
-  ETag: string;
-  Size: number;
-  StorageClass: string;
-}
+import Link from "next/link";
 
 const Portfolio = async () => {
   // Fetch data from S3
@@ -19,21 +13,23 @@ const Portfolio = async () => {
   return (
     <div>
       <h1>My Photography Portfolio</h1>
-      <div className="flex flex-col gap-5 items-center border-2 p-5">
+      <div className="flex flex-col gap-5 items-center p-5">
         {folders?.map((folder, index) => (
-          <div
-            key={index}
-            className="relative w-full h-64 p-2 border-2 border-black text-center rounded-lg"
-          >
-            <div className="absolute inset-0">
-              <Image
-                src="https://images.pexels.com/photos/9956949/pexels-photo-9956949.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                alt="Descrizione dell'immagine"
-                layout="fill"
-                objectFit="cover"
-              />
-            </div>
-            <h2>{folder.Key?.split("/").filter(Boolean).pop()}</h2>
+          <div key={index} className="relative w-full h-64 p-2 text-center">
+            <Link href={"/" + folder.Key?.toLocaleLowerCase()}>
+              <div className="absolute inset-0">
+                <Image
+                  src="https://images.pexels.com/photos/9956949/pexels-photo-9956949.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                  alt="Descrizione dell'immagine"
+                  fill
+                  objectFit="cover"
+                  className="rounded-lg"
+                />
+              </div>
+              <h2 className="absolute inset-0 flex items-center justify-center text-white font-bold z-10">
+                {folder.Key?.split("/").filter(Boolean).pop()}
+              </h2>
+            </Link>
           </div>
         ))}
       </div>
