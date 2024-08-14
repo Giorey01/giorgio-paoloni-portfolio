2f75945c29c1083b7c48ff3e0eda0f292c575bfd
Giorey01 <giorey2001@gmail.com>
2024-08-14 10:43:20 +0200

    Add plaiceholder logic to getBase64 data to blur images previews

--- src/app/portfolio/[slug]/page.tsx
+++ src/app/portfolio/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { getImagesFromFolder } from "@/utils/awsS3UtilityFunctions";
 import Image from "next/image";
+import { getPlaiceholder } from "plaiceholder";
 
 interface PortfolioPageProps {
   params: {
@@ -14,19 +15,40 @@ const PortfolioPage = async ({ params }: PortfolioPageProps) => {
   const images = await getImagesFromFolder(
     "Portfolio/" + decodedSlug.charAt(0).toUpperCase() + decodedSlug.slice(1)
   );
-  console.log(images);
+
+  // Genera i placeholder per le immagini
+  const imagesWithPlaceholder = await Promise.all(
+    (images ?? []).map(async (image) => {
+      const imageUrl = `https://d321io5nxf2wuu.cloudfront.net/${image.Key}`;
+
+      // Recupera l'immagine come un buffer
+      const response = await fetch(imageUrl);
+      const buffer = await response.arrayBuffer();
+
+      // Ottieni il placeholder utilizzando il buffer
+      const { base64 } = await getPlaiceholder(Buffer.from(buffer));
+
+      return {
+        ...image,
+        imgUrl: imageUrl,
+        placeholder: base64,
+      };
+    })
+  );
+
   return (
     <div className="p-4 grid grid-cols-1 sm:grid-cols-2 gap-4">
-      {images?.map((image, index) => (
+      {imagesWithPlaceholder?.map((image, index) => (
         <div key={index} className="relative w-full overflow-hidden rounded-lg">
           <Image
-            key={index}
-            src={`https://giorgio-paoloni-gallery-storage.s3.eu-north-1.amazonaws.com/${image.Key}`}
+            src={image.imgUrl}
             alt={image.Key ?? ""}
             width={800}
-            height={800}
+            height={400}
             className="rounded-md object-cover w-full h-full"
-          ></Image>
+            placeholder="blur"
+            blurDataURL={image.placeholder}
+          />
         </div>
       ))}
     </div>
