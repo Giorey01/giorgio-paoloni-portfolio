727938e4145d617362326095b52359056309bba3
Giorey01 <giorey2001@gmail.com>
2024-08-14 13:20:08 +0200

    Small change to layout

--- src/app/portfolio/[slug]/page.tsx
+++ src/app/portfolio/[slug]/page.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { getImagesFromFolder } from "@/utils/awsS3UtilityFunctions";
 import Image from "next/image";
-import { getPlaiceholder } from "plaiceholder";
 
 interface PortfolioPageProps {
   params: {
@@ -16,26 +15,6 @@ const PortfolioPage = async ({ params }: PortfolioPageProps) => {
     "Portfolio/" + decodedSlug.charAt(0).toUpperCase() + decodedSlug.slice(1)
   );
 
-  /* // Genera i placeholder per le immagini
-  const imagesWithPlaceholder = await Promise.all(
-    (images ?? []).map(async (image) => {
-      const imageUrl = `https://d321io5nxf2wuu.cloudfront.net/${image.Key}`;
-
-      // Recupera l'immagine come un buffer
-      const response = await fetch(imageUrl);
-      const buffer = await response.arrayBuffer();
-
-      // Ottieni il placeholder utilizzando il buffer
-      const { base64 } = await getPlaiceholder(Buffer.from(buffer));
-
-      return {
-        ...image,
-        imgUrl: imageUrl,
-        placeholder: base64,
-      };
-    })
-  );
-*/
   return (
     <div className="p-4 grid grid-cols-1 sm:grid-cols-2 gap-4">
       {images?.map((image, index) => (
