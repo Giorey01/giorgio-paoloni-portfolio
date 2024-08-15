5ad2722550608ab37eea30b31e62877fb0958c15
Giorey01 <giorey2001@gmail.com>
2024-08-15 20:29:24 +0200

    Add padding to medium and large devices

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -11,7 +11,7 @@ const Portfolio = async () => {
       <h1 className="text-3xl text-center font-bold p-10">
         Dive into my world
       </h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 justify-items-center p-5">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 justify-items-center p-4 md:p-8 lg:p-14">
         {folders?.map((folder, index) => {
           return <PortfolioCard key={index} folder={folder} />;
         })}
