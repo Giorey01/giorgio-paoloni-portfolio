fd8bdb5449a03695d2bd5e8139eb8190f26d08c9
Giorey01 <giorey2001@gmail.com>
2024-08-16 10:28:00 +0200

    Add scale up animation to hero image

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,5 +6,24 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+
+  theme: {
+    extend: {
+      animation: {
+        "scale-up-center":
+          "scale-up-center 0.4s cubic-bezier(0.390, 0.575, 0.565, 1.000)   both",
+      },
+      keyframes: {
+        "scale-up-center": {
+          "0%": {
+            transform: "scale(.5)",
+          },
+          to: {
+            transform: "scale(1)",
+          },
+        },
+      },
+    },
+  },
 };
 export default config;
