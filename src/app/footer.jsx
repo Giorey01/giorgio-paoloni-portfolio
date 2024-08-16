7db34b72528aace4f5d44879bd43dd0239bd8feb
Giorey01 <giorey2001@gmail.com>
2024-08-16 10:28:22 +0200

    Adjust footer

--- src/app/footer.jsx
+++ src/app/footer.jsx
@@ -2,12 +2,10 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="flex justify-center items-center w-full gap-12 absolute bottom-0 h-12 text-xs font-medium">
+    <footer className="flex px-6  justify-between md:justify-center items-center w-full gap-12 absolute bottom-0 h-12 text-xs font-medium">
       <p>©2024 Giorgio Paoloni™</p>
       <ul className="flex gap-3">
         <li>Privacy Policy</li>
-        <li>Licensing</li>
-        <li>Contact</li>
       </ul>
     </footer>
   );
